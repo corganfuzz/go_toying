0000d8518c2322d8765b7bdea61386fbc74c33b2
corganfuzz <gruizcavero@uh.edu>
2019-06-03 01:09:38 -0500

    begining of range

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -6,12 +6,18 @@ func main() {
 
 	// Define map
 
-	emails := make(map[string]string)
+	// emails := make(map[string]string)
 
 	// Assign kv
 
-	emails["Bob"] = "bob@gmail.com"
-	emails["Sharon"] = "sharon@gmail.com"
+	// emails["Bob"] = "bob@gmail.com"
+	// emails["Sharon"] = "sharon@gmail.com"
+	// emails["Mike"] = "mike@gmail.com"
+
+	//Declare map and add kv
+
+	emails := map[string]string{"Bob": "bob@gmail.com", "Sharon": "sharon@gmail.com"}
+
 	emails["Mike"] = "mike@gmail.com"
 
 	fmt.Println(len(emails))
