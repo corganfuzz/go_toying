b70b5bca5588b2ccb5b3161c5bb8079fcbfbe1c8
Gerald Ruiz <gruiz@Geralds-MacBook-Pro.local>
2019-06-15 22:23:08 -0500

    wait group added

--- 16_chanels/main.go
+++ 16_chanels/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var wg = &sync.WaitGroup{} //Create a global waitgroup
+
 func runLoopSend(n int, ch chan int) {
+
+	defer wg.Done() //ensure wait group counter decrements by one after our function exits
+
 	for i := 0; i < n; i++ {
 		ch <- i // sending the value i to a channel "ch"
 	}
@@ -13,6 +19,9 @@ func runLoopSend(n int, ch chan int) {
 }
 
 func runLoopReceived(ch chan int) {
+
+	defer wg.Done() //ensure wait group counter decrements after our function exits
+
 	for {
 		i, ok := <-ch // check if the channel is closed
 		// if channel is closed value is false, "i gets its value sent from channel
@@ -28,9 +37,13 @@ func main() {
 
 	myChannel := make(chan int)
 
+	wg.Add(2) // Increment the wait group internal counter by 2
+
 	go runLoopSend(10, myChannel)
 
 	go runLoopReceived(myChannel)
+
+	wg.Wait() // Wait till the wait group counter is 0
 
 	time.Sleep(2 * time.Second)
 }
